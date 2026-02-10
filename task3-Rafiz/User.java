a154b26de66ce103eef29f36ee8d580018c997dc
Rafiz Khan <rafizkhan164@gmail.com>
2026-02-10 18:36:02 +0000

    new classes-rafiz

---
+++ task3-Rafiz/User.java
@@ -0,0 +1,13 @@
+public class User{
+    String user;
+    String password;
+
+
+public void Username(String args){
+    user = args;
+}
+
+public void Password(String args){
+    password = args;
+}
+}
